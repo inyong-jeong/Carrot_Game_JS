3f5e8bc88a3f1594d399f8b9b049151c58807d7b
inyong <jeong4726@gmail.com>
2020-10-08 12:54:10 +0900

    JS:sound add

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ const popUp = document.querySelector(".pop-up");
 const popUpText = document.querySelector(".pop-up__message");
 const popUpRefresh = document.querySelector(".pop-up__refresh");
 
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -39,6 +45,7 @@ function startGame() {
   showStopButton();
   showTimerAndScore();
   startGameTimer();
+  playSound(bgSound);
 }
 
 function showTimerAndScore() {
@@ -70,16 +77,18 @@ function updateTimerText(time) {
 }
 function stopGame() {
   started = false;
-
   stopGameTimer();
   hideGameButton();
   showPopWithText("REPLAY?");
+  playSound(alertSound);
+  stopSound(bgSound);
 }
 
 function showStopButton() {
   const icon = gameBtn.querySelector(".fas");
   icon.classList.add("fa-stop");
   icon.classList.remove("fa-play");
+  gameBtn.style.visibility = "visible";
 }
 
 function hideGameButton() {
@@ -95,6 +104,7 @@ function hidePopUp() {
 }
 function initGame() {
   //벌레와 당근을 생성한 뒤 field에 추가해줌
+  score = 0;
   field.innerHTML = "";
   gameScore.innerText = CARROT_COUNT;
   console.log(fieldRect);
@@ -111,22 +121,35 @@ function onFieldClick(event) {
     //당근
     target.remove();
     score++;
+    playSound(carrotSound);
     updateScoreBoard();
     if (score === CARROT_COUNT) {
       finishGame(true);
     }
   } else if (target.matches(".bug")) {
     //벌레!!
-    stopGameTimer();
     finishGame(false);
   }
 }
 function finishGame(win) {
   started = false;
   hideGameButton();
+  if (win) {
+    playSound(winSound);
+  } else {
+    playSound(bugSound);
+  }
+  stopGameTimer();
+  stopSound(bgSound);
   showPopWithText(win ? "YOU WON" : "YOU LOST");
 }
-
+function playSound(sound) {
+  sound.currentTime = 0;
+  sound.play();
+}
+function stopSound(sound) {
+  sound.pause();
+}
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
 }
